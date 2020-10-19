0cca7ba7c4635a7f6c7b81dd4fc24b556283b454
Taiki Endo <te316e89@gmail.com>
2020-10-19 23:13:31 +0900

    scripts: Add test support to check-minimal-versions.sh

--- scripts/check-minimal-versions.sh
+++ scripts/check-minimal-versions.sh
@@ -3,7 +3,7 @@
 # Check all public crates with minimal version dependencies.
 #
 # Usage:
-#    bash scripts/check-minimal-versions.sh
+#    bash scripts/check-minimal-versions.sh [check|test]
 #
 # Note:
 # - This script modifies Cargo.toml and Cargo.lock while running
@@ -16,17 +16,29 @@ set -euo pipefail
 
 cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
+# Decide Rust toolchain.
+# If the `CI` environment variable is not set to `true`, then nightly is used by default.
 if [[ "${1:-none}" == "+"* ]]; then
     toolchain="${1}"
+    shift
 elif [[ "${CI:-false}" != "true" ]]; then
     cargo +nightly -V >/dev/null || exit 1
     toolchain="+nightly"
 fi
-
+# This script requires nightly Rust and cargo-hack
 if [[ "${toolchain:-+nightly}" != "+nightly"* ]] || ! cargo hack -V &>/dev/null; then
     echo "error: check-minimal-versions.sh requires nightly Rust and cargo-hack"
     exit 1
 fi
+
+subcmd="${1:-check}"
+case "${subcmd}" in
+    check | test) ;;
+    *)
+        echo "error: invalid argument \`${1}\`"
+        exit 1
+        ;;
+esac
 
 # This script modifies Cargo.toml and Cargo.lock, so make sure there are no
 # unstaged changes.
@@ -34,11 +46,13 @@ git diff --exit-code
 # Restore original Cargo.toml and Cargo.lock on exit.
 trap 'git checkout .' EXIT
 
-# Remove dev-dependencies from Cargo.toml to prevent the next `cargo update`
-# from determining minimal versions based on dev-dependencies.
-cargo hack --remove-dev-deps --workspace
+if [[ "${subcmd}" == "check" ]]; then
+    # Remove dev-dependencies from Cargo.toml to prevent the next `cargo update`
+    # from determining minimal versions based on dev-dependencies.
+    cargo hack --remove-dev-deps --workspace
+fi
 
 # Update Cargo.lock to minimal version dependencies.
 cargo ${toolchain:-} update -Zminimal-versions
 # Run check for all public members of the workspace.
-cargo ${toolchain:-} hack check --workspace --all-features --ignore-private -Zfeatures=all
+cargo ${toolchain:-} hack "${subcmd}" --workspace --all-features --ignore-private -Zfeatures=all
