dd493c3f37bdcc142694b615c4533c61d9c1aea2
Taiki Endo <te316e89@gmail.com>
2020-11-09 07:00:02 +0900

    ci: Distribute compiled binary files via GitHub release

---
+++ ci/upload-assets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+ref="${GITHUB_REF:?}"
+tag="${ref#*/tags/}"
+
+export CARGO_PROFILE_RELEASE_LTO=true
+host=$(rustc -Vv | grep host | sed 's/host: //')
+
+package="cargo-hack"
+cargo build --bin "${package}" --release
+
+cd target/release
+case "${OSTYPE}" in
+  linux* | darwin*)
+    asset="${package}-${tag}-${host}.tar.gz"
+    tar czf ../../"${asset}" "${package}"
+    ;;
+  cygwin* | msys*)
+    asset="${package}-${tag}-${host}.zip"
+    7z a ../../"${asset}" "${package}".exe
+    ;;
+  *)
+    echo "unrecognized OSTYPE: ${OSTYPE}"
+    exit 1
+    ;;
+esac
+cd ../..
+
+if [[ -z "${GITHUB_TOKEN:-}" ]]; then
+  echo "GITHUB_TOKEN not set, skipping deploy"
+  exit 1
+else
+  gh release upload "${tag}" "${asset}" --clobber
+fi
