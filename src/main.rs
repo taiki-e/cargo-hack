48ff93d2274ca765372400792f2665112ba0b8d8
Taiki Endo <te316e89@gmail.com>
2020-12-23 01:26:46 +0900

    Update allowed lints

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![forbid(unsafe_code)]
 #![warn(future_incompatible, rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take requires Rust 1.40
-#![allow(clippy::mem_replace_with_default)]
 
 #[macro_use]
 mod term;
