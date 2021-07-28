90d606a2cfe450646239276ad51fd35a649d5d6c
Taiki Endo <te316e89@gmail.com>
2021-07-29 05:22:53 +0900

    Remove closure from print_inner

--- src/term.rs
+++ src/term.rs
@@ -1,6 +1,6 @@
 use std::{
     env,
-    io::{self, Write},
+    io::Write,
     sync::atomic::{AtomicU8, Ordering::Relaxed},
 };
 
@@ -35,36 +35,44 @@ fn coloring() -> ColorChoice {
     }
 }
 
-pub(crate) fn print_inner(
-    color: Option<Color>,
-    kind: &str,
-    write_msg: impl FnOnce(&mut StandardStream) -> io::Result<()>,
-) {
+pub(crate) fn print_inner(color: Option<Color>, kind: &str) -> StandardStream {
     let mut stream = StandardStream::stderr(coloring());
     let _ = stream.set_color(ColorSpec::new().set_bold(true).set_fg(color));
     let _ = write!(stream, "{}", kind);
     let _ = stream.reset();
     let _ = write!(stream, ": ");
-    let _ = write_msg(&mut stream);
+    stream
 }
 
 macro_rules! error {
     ($($msg:expr),* $(,)?) => {{
         use std::io::Write;
-        crate::term::print_inner(Some(termcolor::Color::Red), "error", |stream| writeln!(stream, $($msg),*));
+        let mut stream = crate::term::print_inner(
+            Some(termcolor::Color::Red),
+            "error",
+        );
+        let _ = writeln!(stream, $($msg),*);
     }};
 }
 
 macro_rules! warn {
     ($($msg:expr),* $(,)?) => {{
         use std::io::Write;
-        crate::term::print_inner(Some(termcolor::Color::Yellow), "warning", |stream| writeln!(stream, $($msg),*));
+        let mut stream = crate::term::print_inner(
+            Some(termcolor::Color::Yellow),
+            "warning",
+        );
+        let _ = writeln!(stream, $($msg),*);
     }};
 }
 
 macro_rules! info {
     ($($msg:expr),* $(,)?) => {{
         use std::io::Write;
-        crate::term::print_inner(None, "info", |stream| writeln!(stream, $($msg),*));
+        let mut stream = crate::term::print_inner(
+            None,
+            "info",
+        );
+        let _ = writeln!(stream, $($msg),*);
     }};
 }
