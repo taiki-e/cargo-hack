a64e0ae5c5a8d7fdcdc6342474d9e282e7080131
Taiki Endo <te316e89@gmail.com>
2022-10-25 23:06:13 +0900

    Fix clippy::uninlined_format_args warning
    ```
    error: variables can be used directly in the `format!` string
     --> tests/auxiliary/build-info/build.rs:7:25
      |
    7 |     fs::write(out_file, format!("\"{}\"", target))
      |                         ^^^^^^^^^^^^^^^^^^^^^^^^^
      |
      = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#uninlined_format_args
      = note: `-D clippy::uninlined-format-args` implied by `-D warnings`
    help: change this to
      |
    7 -     fs::write(out_file, format!("\"{}\"", target))
    7 +     fs::write(out_file, format!("\"{target}\""))
      |
    
    error: variables can be used directly in the `format!` string
     --> tests/auxiliary/build-info/build.rs:8:29
      |
    8 |         .unwrap_or_else(|e| panic!("failed to write {}: {}", out_file.display(), e));
      |                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      |
      = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#uninlined_format_args
    help: change this to
      |
    8 -         .unwrap_or_else(|e| panic!("failed to write {}: {}", out_file.display(), e));
    8 +         .unwrap_or_else(|e| panic!("failed to write {}: {e}", out_file.display()));
      |
    ```

--- tests/auxiliary/build-info/build.rs
+++ tests/auxiliary/build-info/build.rs
@@ -4,6 +4,6 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     let out_dir: PathBuf = env::var_os("OUT_DIR").expect("OUT_DIR not set").into();
     let out_file = &out_dir.join("target");
-    fs::write(out_file, format!("\"{}\"", target))
-        .unwrap_or_else(|e| panic!("failed to write {}: {}", out_file.display(), e));
+    fs::write(out_file, format!("\"{target}\""))
+        .unwrap_or_else(|e| panic!("failed to write {}: {e}", out_file.display()));
 }
