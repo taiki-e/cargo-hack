479d31d4f7bae9a8dc394cbce51492a7c61c1df3
Taiki Endo <te316e89@gmail.com>
2022-11-27 20:50:13 +0900

    Tweak build script

--- tests/auxiliary/build-info/build.rs
+++ tests/auxiliary/build-info/build.rs
@@ -1,7 +1,7 @@
 use std::{env, fs, path::PathBuf};
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
+    let target = env::var("TARGET").expect("TARGET not set");
     let out_dir: PathBuf = env::var_os("OUT_DIR").expect("OUT_DIR not set").into();
     let out_file = &out_dir.join("target");
     fs::write(out_file, format!("\"{target}\""))
