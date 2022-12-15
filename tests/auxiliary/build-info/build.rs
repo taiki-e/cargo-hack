3fd9eac4b5c4f287a9b3a0de63c53c9eeb88f918
Taiki Endo <te316e89@gmail.com>
2022-12-16 02:55:19 +0900

    Set rerun-if-changed in helper crate's build script

--- tests/auxiliary/build-info/build.rs
+++ tests/auxiliary/build-info/build.rs
@@ -1,6 +1,8 @@
 use std::{env, fs, path::PathBuf};
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
     let target = env::var("TARGET").expect("TARGET not set");
     let out_dir: PathBuf = env::var_os("OUT_DIR").expect("OUT_DIR not set").into();
     let out_file = &out_dir.join("target");
