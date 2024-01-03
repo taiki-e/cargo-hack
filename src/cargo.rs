020d5f0126f4657505869b0195132773901a4048
Taiki Endo <te316e89@gmail.com>
2024-01-04 01:57:43 +0900

    Clean up cargo version parsing

--- src/cargo.rs
+++ src/cargo.rs
@@ -4,25 +4,19 @@ use anyhow::{bail, format_err, Result};
 
 use crate::{version::Version, ProcessBuilder};
 
-// The version detection logic is based on https://github.com/cuviper/autocfg/blob/1.0.1/src/version.rs#L25-L59
 pub(crate) fn version(mut cmd: ProcessBuilder<'_>) -> Result<Version> {
+    // Use verbose version output because the packagers add extra strings to the normal version output.
     cmd.args(["--version", "--verbose"]);
-    let output = cmd.read()?;
-
-    // Find the release line in the verbose version output.
-    let release =
-        output.lines().find_map(|line| line.strip_prefix("release: ")).ok_or_else(|| {
-            format_err!("could not find rustc release from output of {cmd}: {output}")
-        })?;
-
-    // Split the version and channel info.
-    let mut version_channel = release.split('-');
-    let version = version_channel.next().unwrap();
-    let _channel = version_channel.next();
+    let verbose_version = cmd.read()?;
+    let release = verbose_version
+        .lines()
+        .find_map(|line| line.strip_prefix("release: "))
+        .ok_or_else(|| format_err!("unexpected output from {cmd}: {verbose_version}"))?;
+    let (version, _channel) = release.split_once('-').unwrap_or((release, ""));
 
     let version: Version = version.parse()?;
     if version.major != 1 || version.patch.is_none() {
-        bail!("unexpected output from {cmd}: {output}");
+        bail!("unexpected output from {cmd}: {verbose_version}");
     }
 
     Ok(version)
