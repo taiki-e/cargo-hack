b74af518d370dff44fc1301142fac07ec3364912
Taiki Endo <te316e89@gmail.com>
2025-01-14 02:39:02 +0900

    Tweak internal docs

--- src/fs.rs
+++ src/fs.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 use anyhow::{Context as _, Result};
 
-/// Write a slice as the entire contents of a file.
+/// Writes a slice as the entire contents of a file.
 /// This is a wrapper for [`std::fs::write`].
 pub(crate) fn write(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Result<()> {
     let path = path.as_ref();
@@ -12,7 +12,7 @@ pub(crate) fn write(path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Resul
     res.with_context(|| format!("failed to write to file `{}`", path.display()))
 }
 
-/// Read the entire contents of a file into a string.
+/// Reads the entire contents of a file into a string.
 /// This is a wrapper for [`std::fs::read_to_string`].
 pub(crate) fn read_to_string(path: impl AsRef<Path>) -> Result<String> {
     let path = path.as_ref();
