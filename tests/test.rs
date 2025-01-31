4b5c50fc60756243d32ff9402cbad689ccfbc407
Taiki Endo <te316e89@gmail.com>
2025-02-01 06:31:49 +0900

    tests: Use doc::sync_command_output_to_markdown from test-helper

--- tests/test.rs
+++ tests/test.rs
@@ -10,7 +10,6 @@ use std::{
     sync::Mutex,
 };
 
-use fs_err as fs;
 use test_helper::git::assert_diff;
 
 use self::auxiliary::*;
@@ -2027,37 +2026,8 @@ fn version() {
 
 #[test]
 fn update_readme() {
-    let new = &*test_helper::cli::CommandExt::assert_success(&mut cargo_hack(["--help"])).stdout;
-    let path = &Path::new(env!("CARGO_MANIFEST_DIR")).join("README.md");
-    let base = fs::read_to_string(path).unwrap();
-    let mut out = String::with_capacity(base.capacity());
-    let mut lines = base.lines();
-    let mut start = false;
-    let mut end = false;
-    while let Some(line) = lines.next() {
-        out.push_str(line);
-        out.push('\n');
-        if line == "<!-- readme-long-help:start -->" {
-            start = true;
-            out.push_str("```console\n");
-            out.push_str("$ cargo hack --help\n");
-            out.push_str(new);
-            for line in &mut lines {
-                if line == "<!-- readme-long-help:end -->" {
-                    out.push_str("```\n");
-                    out.push_str(line);
-                    out.push('\n');
-                    end = true;
-                    break;
-                }
-            }
-        }
-    }
-    if start && end {
-        assert_diff(path, out);
-    } else if start {
-        panic!("missing `<!-- readme-long-help:end -->` comment in README.md");
-    } else {
-        panic!("missing `<!-- readme-long-help:start -->` comment in README.md");
-    }
+    let new = test_helper::cli::CommandExt::assert_success(&mut cargo_hack(["--help"])).stdout;
+    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("README.md");
+    let command = "cargo hack --help";
+    test_helper::doc::sync_command_output_to_markdown(path, "readme-long-help", command, new);
 }
